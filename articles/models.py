baf80cef18bddb60ba07a09da816ba7e90b5f486
Mariam <Mariamhh98@hotmail.com>
2019-12-11 21:39:44 +0300

    Done with list and detail page

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from datetime import datetime
+
+
+class Article(models.Model):
+    owner = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+    title = models.CharField(max_length=500)
+    content = models.TextField()
+    date = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.title
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Article
 
-# Create your views here.
+def article_list(request):
+    articles = Article.objects.all().order_by('-id')
+    context = {
+        "articles": articles,
+    }
+    return render(request, 'list.html', context)
+
+
+def article_detail(request, article_id):
+    article = Article.objects.get(id=article_id)
+    context = {
+        "article": article,
+    }
+    return render(request, 'detail.html', context)
